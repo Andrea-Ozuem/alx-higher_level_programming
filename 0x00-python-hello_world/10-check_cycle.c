9024f771fca1546504c8fba112f69396071295d4
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-11-28 16:54:15 +0000

    Create task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "lists.h"
+
+/**
+  *check_cycle - checks if a singly linked list has a cycle in it
+  *@list: list head
+  *Return: 1 if has a cycle else 0
+  */
+int check_cycle(listint_t *list)
+{
+	listint_t *p, *tmp;
+
+	p = malloc(sizeof(listint_t));
+	if (p == NULL)
+		return (0);
+	p = list;
+	tmp = list;
+	p = p->next;
+	while (p != NULL)
+	{
+		if (p == tmp)
+			return (1);
+		p = p->next;
+	}
+	return (0);
+}
