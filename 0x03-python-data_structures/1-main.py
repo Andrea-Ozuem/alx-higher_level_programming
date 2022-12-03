3cc9209b3428a7321112fda1d1e2d5e72e3e190e
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-12-03 19:00:43 +0100

    testing

--- 0x03-python-data_structures/1-main.py
+++ 0x03-python-data_structures/1-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 element_at = __import__('1-element_at').element_at
 
-my_list = []
-idx = 0
+my_list = ["hey", "hi", "girl"]
+idx = 2
 print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if my_list == []:
+        return my_list
     if idx < 0:
         return None
     if idx > len(my_list):
