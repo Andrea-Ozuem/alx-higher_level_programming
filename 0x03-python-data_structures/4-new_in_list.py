fbde5eddb03132f87521dff2ef93f20dabcc2f46
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-12-03 19:47:27 +0100

    Fix empty string output

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     n_list = my_list.copy()
-    if idx >= 0 and idx < len(n_list)
-        n_list[idx] = element
+    if my_list:
+        if idx >= 0 and idx < len(n_list):
+            n_list[idx] = element
     return n_list
