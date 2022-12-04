9b3c78f68a55d0080d466dadddf9f08951e34d0c
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-12-04 11:23:38 +0100

    Ensure pycodestyle format

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-        tuple_a = a[0], 0
-    n_tup = a[0] + b[0], a[1] + b[1]
-    return n_tup
+#    if len(tuple_a) < 2:
+#       tuple_a = a[0], 0
+    result = tuple(map(sum, zip(tuple_a, tuple_b)))
+    return result
