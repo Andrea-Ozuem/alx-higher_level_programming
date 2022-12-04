5257ebb0e2e9d2734e52898af6e14abb239927d3
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-12-04 11:28:30 +0100

    fix empty list case

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return None
-    max_iem = my_list[0]
+    max_item = my_list[0]
     for i in my_list:
         if max_item < i:
             max_item = i
