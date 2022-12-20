f14b793722551296a030d6b7b58d08bd430bf5af
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-12-21 00:32:27 +0100

    Ensure PEP8 style

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -16,5 +16,12 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
+        """Return Area of Square instance
+
+        Returnl:
+            area of square
+        """
+
         return (self.__size)**2
