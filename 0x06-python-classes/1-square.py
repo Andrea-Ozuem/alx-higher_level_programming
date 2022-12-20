de16782aaa7ea5272a84608cbf446e03061a7a43
Andrea-Ozuem <andreaozuem2021@gmail.com>
2022-12-20 23:40:58 +0100

    Create

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Creates empty square class"""
+
+
+class Square:
+    """ A square class"""
+    def __init__(self, size):
+        """INitialise sqaure instance
+
+        Args:
+            size: Size of square
+
+        """
+        self.__size = size
