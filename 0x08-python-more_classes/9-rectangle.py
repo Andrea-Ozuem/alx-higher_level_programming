78d38c5776bbc8ea3c8392042232572e2d04abe3
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-01-02 23:38:28 +0100

    You pushed me!

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,52 +1,45 @@
 #!/usr/bin/python3
-""" Create C Rectangle Class"""
+"""
+This module is composed by a class that defines a Rectangle
+"""
 
 
 class Rectangle:
-    """ Creates class
-        Attributes:
-        number_of_instances (int): The number of Rectangle instances.
-        print_symbol (any): The symbol used for string representation.
-    """
+    """ Class that defines a rectangle """
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """ Initialise instance with width and height
-            Args:
-                width (int): The width of the new rectangle.
-                height (int): The height of the new rectangle.
+        """ Method that initializes the instance
+        Args:
+            width: rectangle width
+            height: rectangle height
         """
-        self.height = height
+
         self.width = width
-        type(self).number_of_instances += 1
-
-    def __str__(self):
-        """returns printable representaion of object"""
-        rectangle = ""
-        if self.width == 0 or self.height == 0:
-            return rectangle
-        for i in range(self.height):
-            rectangle += (str(self.print_symbol) * self.width) + "\n"
-        return rectangle[:-1]
-
-    def __repr__(self):
-        """ Returns string representation of class"""
-        return f"Rectangle({self.__width}, {self.__height})"
-
-    def __del__(self):
-        """ Deletes instance"""
-        type(self).number_of_instances -= 1
-        print("Bye rectangle...")
+        self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """int:  Gets/sets Width of object"""
+        """ method that returns the value of the width
+        Returns:
+            rectangle width
+        """
+
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ method that defines the width
+        Args:
+            value: width
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width is less than zero
+        """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -55,11 +48,23 @@ class Rectangle:
 
     @property
     def height(self):
-        """ int: Gets/sets height of object"""
+        """ method that returns the value of the height
+        Returns:
+            rectangle height
+        """
+
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ method that defines the height
+        Args:
+            value: height
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height is less than zero
+        """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -67,28 +72,86 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """ Returns Rectangle area"""
-        return self.__width * self.__height
+        """ Method that calculates the Rectangle area
+        Returns:
+            rectangle area
+        """
+
+        return self.width * self.height
 
     def perimeter(self):
-        """ returns Rectangle perimeter"""
-        if self.__width == 0 or self.__height == 0:
+        """ Method that calculates the Rectangle perimeter
+        Returns:
+            rectangle perimeter
+        """
+
+        if self.width == 0 or self.height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+
+        return (2 * self.width) + (2 * self.height)
+
+    def __str__(self):
+        """ Method that returns the Rectangle #
+        Returns:
+            str of the rectangle
+        """
+
+        rectangle = ""
+
+        if self.width == 0 or self.height == 0:
+            return rectangle
+
+        for i in range(self.height):
+            rectangle += (str(self.print_symbol) * self.width) + "\n"
+
+        return rectangle[:-1]
+
+    def __repr__(self):
+        """ Method that returns the string represantion of the instance
+        Returns:
+            string represenation of the object
+        """
+
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
+
+    def __del__(self):
+        """ Method that prints a message when the instance is deleted
+        """
+
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """ Compares  2 Rectangle objects"""
+        """ Method that returns the bigger Rectangle
+        Args:
+            rect_1: Rectangle 1
+            rect_2: Rectangle 2
+        Raises:
+            TypeError: when some argument passed is not
+            an instance of the Rectangle class
+        Returns:
+            The bigger Rectangle
+        """
+
         if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance")
         if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() < rect_2.area():
-            return rect_2
-        elif rect_1.area() >= rect_2.area():
+            raise TypeError("rect_2 must be an instance")
+
+        if rect_1.area() >= rect_2.area():
             return rect_1
+        else:
+            return rect_2
 
     @classmethod
     def square(cls, size=0):
-        """ Returns a new Rectangle instance"""
+        """ Method that returns a new instance of Rectangle class
+        Args:
+            cls: rectangle class
+            size: rectangle width and rectangle height
+        Returns:
+            a new instance of Rectangle class
+        """
+
         return cls(size, size)
