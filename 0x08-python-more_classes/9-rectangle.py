ae13306404e635bc53b2b868a7db745db7e12d62
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-01-02 23:23:31 +0100

    docstring line lenght

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -92,5 +92,5 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """ Returns a new Rectangle instance with width == height == size"""
-        return Rectangle(size, size)
+        """ Returns a new Rectangle instance"""
+        return cls(size, size)
