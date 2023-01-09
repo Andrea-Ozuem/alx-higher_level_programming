032d372f0e1816a83f60049b30a8fe608a5d506b
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-01-09 23:26:54 +0100

    create

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,9 +8,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """Creates Rectangle subclass"""
     def __init__(self, size):
-        self.integer_validator("size", width)
+        self.integer_validator("size", size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
 
     def area(self):
         """ Method that returns the area of the instance"""
-        return self.__size ** 2
+        return self.__size * self.__size
