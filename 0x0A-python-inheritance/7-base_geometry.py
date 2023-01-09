af021c23c6549eefd63c1fb89493caee0dfb36cf
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-01-09 18:48:04 +0100

    no

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Create class"""
+
+
+class BaseGeometry:
+    """Create geometry calss"""
+    def area(self):
+        """Raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+    
