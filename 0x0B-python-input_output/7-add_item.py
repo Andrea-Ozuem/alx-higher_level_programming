19348c5cada9cbbb4110de3ccfd2cd6e03f7dbfd
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-01-10 19:49:01 +0100

    fix unknown

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,21 +4,15 @@
 """
 
 from sys import argv
-from os.path import exists
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 file_n = "add_item.json"
-py_list = []
-
-if not exists(file_n):
-    with open(file_n, "w", encoding="utf-8") as f:
-        pass
-
-curr = load_from_json_file(file_n)
-for item in curr:
-    py_list.append(item)
+try:
+    curr = load_from_json_file(file_n)
+except FileNotFoundError:
+    curr = []
 
 for i in range(1, len(argv)):
-    py_list.append(argv[i])
-save_to_json_file(py_list, file_n)
+    curr.append(argv[i])
+save_to_json_file(curr, file_n)
