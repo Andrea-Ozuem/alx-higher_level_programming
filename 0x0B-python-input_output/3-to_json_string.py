a4ba395771a5b4602cb94fc4b26fa15ce6678640
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-01-10 18:23:06 +0100

    Enusre Google style guide

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import json
-
 """JSON"""
+import json
 
 
 def to_json_string(my_obj):
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import json
 """Deserialise object"""
+
+import json
 
 
 def from_json_string(my_str):
