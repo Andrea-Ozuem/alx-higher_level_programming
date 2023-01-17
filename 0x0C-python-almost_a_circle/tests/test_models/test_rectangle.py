6bb964c75e1fcd074e9765f081895385c9d4016f
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-01-17 13:37:14 +0100

    Done

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -24,21 +24,7 @@ class TestRectangle(unittest.TestCase):
     def test_instance(self):
         """Test instantiation
         """
-
-        o1 = Rectangle(3, 2)
-        o2 = Rectangle(8, 7, 0, 0, 12)
-        with self.assertRaises(TypeError):
-            o3 = Rectangle("string")
-            o4 = Rectangle(None)
-            o5 = Rectangle(float('inf'))
-            o6 = Rectangle(9.5, 9.3)
-            o7 = Rectangle(-8, 9)
-            o8 = Rectangle()
-
-        self.assertEqual(o1.id, 1)
-        self.assertEqual(o1._Base__nb_objects, 1)
-        self.assertEqual(o2.id, 12)
-        self.assertEqual(o2._Base__nb_objects, 1)
+        self.assertIsInstance(Rectangle(10, 2), Base)
 
     def test_area(self):
         """Testing area()
