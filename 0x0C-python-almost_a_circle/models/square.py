3659bf620a8811122b0c12851103fe43918a9f50
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-01-17 13:18:13 +0100

    redid

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the Square.
+        Args:
+            *args (ints): New attribute values.
+        """
+        att = ["id", "size", "x", "y"]
+        if len(args) != 0:
+            i = 0
+            try:
+                for arg in args:
+                    setattr(self, att[i], arg)
+                    i += 1
+            except IndexError:
+                pass
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Rectangle"""
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
