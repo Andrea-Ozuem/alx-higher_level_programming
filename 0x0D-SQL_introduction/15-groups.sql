3fe471c64e21ee6c607ff80f4c24ed39b387657f
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-02-15 01:22:03 +0100

    ok

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
--- lists the number of records with the same score in the table second_table
+-- lists the number of records with the same score in second_table
+-- the score, the number of records for this score with the label number
 SELECT score, COUNT(*) AS "number" FROM second_table GROUP BY score;
