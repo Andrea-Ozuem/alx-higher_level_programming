ce37d913d510d827a5b62a5ffebeb95087b803ff
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-02-16 00:42:39 +0100

    Add semi colon

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,8 @@
 -- cities description: id INT unique, auto generated, can't be null and is a primary key
 -- name VARCHAR(256) can't be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa
-CREATE TABLE IF NOT EXISTS cities(
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
