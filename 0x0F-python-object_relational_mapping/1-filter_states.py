0cc5f7a28008f236d78e2b616a060f63fc62af79
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-17 20:30:19 +0100

    Ensure text matching is case sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,6 +2,7 @@
 
 """a script that lists all states that starts with 'N' from a database"""
 
+
 import sys
 import MySQLdb
 
@@ -9,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
