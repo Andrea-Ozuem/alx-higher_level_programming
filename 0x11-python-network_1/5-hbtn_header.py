57fd6c1a018e8f0f88ee3e61dc8a327744b84d76
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-31 16:06:07 +0100

    fix: Add script uses requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''fetches heaser var using requests'''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    content = requests.get(sys.argv[1])
+    content = content.headers['X-Request-Id']
+    print(content)
