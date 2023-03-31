3194eb67274b1989a4345b04763128ec9991b4ee
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-31 15:52:30 +0100

    fix: ALX

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''sends a request to the URL displays the body of the response'''
+from urllib import request, error
+import sys
+
 
 if __name__ == "__main__":
-    from urllib import request, error
-    import sys
-
     try:
-        with urllib.request.urlopen(sys.argv[1]) as res:
+        with request.urlopen(sys.argv[1]) as res:
             content = res.read().decode('utf-8')
             print(content)
     except error.HTTPError as err:
