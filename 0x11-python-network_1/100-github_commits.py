9664af8051c3d2eb21c3c70d6913ae4e4f6e8765
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-31 18:43:04 +0100

    fix: Pycodestyle compliance

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,9 +7,7 @@ import sys
 if __name__ == "__main__":
     url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
                                                               sys.argv[1])
-    headers = {'X-GitHub-Api-Version': '2022-11-28',
-               'Accept': 'application/vnd.github+json'}
-    res = requests.get(url, headers=headers)
+    res = requests.get(url)
     try:
         res = res.json()
         for i in range(10):
