ef54410434570cde681bbb5464bf891a9561a042
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-31 18:32:46 +0100

    feat: Backend devpycodestyle 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''sends a github using github api'''
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
+                                                              sys.argv[1])
+    headers = {'X-GitHub-Api-Version': '2022-11-28',
+               'Accept': 'application/vnd.github+json'}
+    res = requests.get(url, headers=headers)
+    try:
+        res = res.json()
+        for i in range(10):
+            print('{}: {}'.format(res[i].get('sha'), res[i]['commit']['author']
+                  .get('name')))
+    except ValueError:
+        pass
