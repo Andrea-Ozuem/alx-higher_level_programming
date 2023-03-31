33ee60ebd368b0a575bd9988c4de1b9b6f0920a6
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-31 15:33:00 +0100

    feat: Work with error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''displays the body of the response'''
+
+if __name__ == "__main__":
+    import urllib.request
+    from urllib.error import URLError, HTTPError
+    import sys
+
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as res:
+            content = res.read().decode('utf-8')
+            print(content)
+    except HTTPError as e:
+        print('Error code:', e.code)
