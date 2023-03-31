5efafb91055ea352f32bb17a6131cb9489d00855
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-31 17:34:08 +0100

    fix: Use get function

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-"""
-Python script that sends a POST request to the URL and
-to an URL with the letter as a parameter
-"""
+'''sends a POST request to url json'''
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    data = {'q': ""}
-
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    res = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
-        data['q'] = sys.argv[1]
-    except:
-        pass
-
-    r = requests.post('http://0.0.0.0:5000/search_user', data)
-
-    try:
-        json_o = r.json()
-        if not json_o:
-            print("No result")
+        res = res.json()
+        if len(res) == 0:
+            print('No result')
         else:
-            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
-    except:
-        print("Not a valid JSON")
+            print("[{}] {}".format(res.get('id'), res.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
