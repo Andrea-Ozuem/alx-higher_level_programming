6ff0ec197445c7e36fa7f9580d198cc4bbafc49b
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-03-31 15:59:40 +0100

    feat: Add script that uses requests package

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+
+
+if __name__ == "__main__":
+    content = requests.get('https://alx-intranet.hbtn.io/status')
+    content = content.text
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
