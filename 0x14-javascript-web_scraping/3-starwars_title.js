367bd220626babfb9e105c64c1b9e2edf29f9f3e
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-26 03:01:41 +0100

    merge' '

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`
-
-request (url, function (error, response, body) {
-  console.log(JSON.parse(body).title);
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request.get(url).on('response', (response) => {
+  console.log(JSON.parse(response).title);
 });
