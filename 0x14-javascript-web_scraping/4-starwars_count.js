360e006c4383f32db412d21bb2624db045bcadf2
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-26 03:31:28 +0100

    feature: FILter results from api

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
+const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(url, function (error, response, body) {
-  console.log(JSON.parse(body).title);
+  if (error) throw error;
+  const results = JSON.parse(body).results;
+
+  const filtered = results.filter(res =>
+    res.characters.includes(charUrl));
+  console.log(filtered.length);
 });
